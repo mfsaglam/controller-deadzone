cfb4e56a6d93a3aed6e9e000b0605da1778ea2c2
mfsaglam <mfsaglam1@gmail.com>
2022-12-10 22:57:49 +0300

    Initial Commit

---
+++ Controller DeadzoneUITests/Controller_DeadzoneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Controller_DeadzoneUITestsLaunchTests.swift
+//  Controller DeadzoneUITests
+//
+//  Created by Fatih Sağlam on 10.12.2022.
+//
+
+import XCTest
+
+final class Controller_DeadzoneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

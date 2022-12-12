6ed86f7c96d48b55271a1447c942e662e786c4d6
mfsaglam <mfsaglam1@gmail.com>
2022-12-12 12:11:02 +0300

    set controller input handler

--- Controller Deadzone/ViewControllers/ThumbsticksViewController.swift
+++ Controller Deadzone/ViewControllers/ThumbsticksViewController.swift
@@ -11,6 +11,7 @@ import GameController
 class ThumbsticksViewController: UIViewController {
     
     var notificationCenter: NotificationCenterProtocol = NotificationCenter.default
+    var gamepad: GCExtendedGamepad?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,56 +23,60 @@ class ThumbsticksViewController: UIViewController {
         // Use the controller object to read input from the game controller.
         
         if let gamepad = controller.extendedGamepad {
+            self.gamepad = gamepad
+            if self.gamepad?.valueChangedHandler == nil {
+                setControllerInputHandler()
+            }
             
-            // Update light settings
+            //TODO: Update light settings
             if #available(iOS 14.0, *) {
                 gamepad.controller?.light?.color = .init(red: 0, green: 0, blue: 0)
             } else {
                 // Fallback on earlier versions
             }
-            
-            if gamepad.valueChangedHandler == nil {
-                controller.extendedGamepad?.valueChangedHandler = { (gamepad: GCExtendedGamepad, element: GCControllerElement) in
-                    switch element {
-                    case gamepad.leftThumbstick:
-                        
-                        if gamepad.leftThumbstick.xAxis.value >= 0 {
-                            print(gamepad.leftThumbstick.xAxis.value)
-                            //Add your function for the right direction
-                        } else if gamepad.leftThumbstick.xAxis.value <= 0 {
-                            print(gamepad.leftThumbstick.xAxis.value)
-                            //Add your function for the left direction
-                        }
-                        
-                        if gamepad.leftThumbstick.yAxis.value >= 0 {
-                            print(gamepad.leftThumbstick.yAxis.value)
-                            //Add your function for the up direction
-                        } else if gamepad.leftThumbstick.yAxis.value <= 0 {
-                            print(gamepad.leftThumbstick.yAxis.value)
-                            //Add your function for the down direction
-                        }
-                    case gamepad.rightThumbstick:
-                        
-                        if gamepad.rightThumbstick.xAxis.value >= 0 {
-                            print(gamepad.rightThumbstick.xAxis.value)
-                            //Add your function for the right direction
-                        } else if gamepad.rightThumbstick.xAxis.value <= 0 {
-                            print(gamepad.rightThumbstick.xAxis.value)
-                            //Add your function for the left direction
-                        }
-                        
-                        if gamepad.rightThumbstick.yAxis.value >= 0 {
-                            print(gamepad.rightThumbstick.yAxis.value)
-                            //Add your function for the up direction
-                        } else if gamepad.rightThumbstick.yAxis.value <= 0 {
-                            print(gamepad.rightThumbstick.yAxis.value)
-                            //Add your function for the down direction
-                        }
-                        
-                    default:
-                        return
-                    }
+        }
+    }
+    
+    private func setControllerInputHandler() {
+        gamepad?.valueChangedHandler = { (gamepad: GCExtendedGamepad, element: GCControllerElement) in
+            switch element {
+            case gamepad.leftThumbstick:
+                
+                if gamepad.leftThumbstick.xAxis.value >= 0 {
+                    print(gamepad.leftThumbstick.xAxis.value)
+                    //Add your function for the right direction
+                } else if gamepad.leftThumbstick.xAxis.value <= 0 {
+                    print(gamepad.leftThumbstick.xAxis.value)
+                    //Add your function for the left direction
                 }
+                
+                if gamepad.leftThumbstick.yAxis.value >= 0 {
+                    print(gamepad.leftThumbstick.yAxis.value)
+                    //Add your function for the up direction
+                } else if gamepad.leftThumbstick.yAxis.value <= 0 {
+                    print(gamepad.leftThumbstick.yAxis.value)
+                    //Add your function for the down direction
+                }
+            case gamepad.rightThumbstick:
+                
+                if gamepad.rightThumbstick.xAxis.value >= 0 {
+                    print(gamepad.rightThumbstick.xAxis.value)
+                    //Add your function for the right direction
+                } else if gamepad.rightThumbstick.xAxis.value <= 0 {
+                    print(gamepad.rightThumbstick.xAxis.value)
+                    //Add your function for the left direction
+                }
+                
+                if gamepad.rightThumbstick.yAxis.value >= 0 {
+                    print(gamepad.rightThumbstick.yAxis.value)
+                    //Add your function for the up direction
+                } else if gamepad.rightThumbstick.yAxis.value <= 0 {
+                    print(gamepad.rightThumbstick.yAxis.value)
+                    //Add your function for the down direction
+                }
+                
+            default:
+                return
             }
         }
     }

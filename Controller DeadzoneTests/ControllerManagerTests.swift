bb11a01f2325a16d304e4fd2b22b1f6678d22207
mfsaglam <mfsaglam1@gmail.com>
2022-12-19 19:22:08 +0300

    add test case for controller manager

---
+++ Controller DeadzoneTests/ControllerManagerTests.swift
@@ -0,0 +1,14 @@
+//
+//  ControllerManagerTests.swift
+//  Controller DeadzoneTests
+//
+//  Created by Fatih Sağlam on 19.12.2022.
+//
+
+import XCTest
+
+final class ControllerManagerTests: XCTestCase {
+    
+    
+
+}
